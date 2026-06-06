c805205df05e555df068920dab18fe6810cdfc94
Shaw <shawmakesmagic@gmail.com>
2026-06-05 22:27:58 -0600

    Autosync working tree state

--- src/components/Hero/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as HomeHero } from "./HomeHero"
-export { default as HubHero } from "./HubHero"
-export { default as MdxHero, type MdxHeroProps } from "./MdxHero"
-export { default as PageHero, type PageHeroProps } from "./PageHero"
--- src/lib/utils/layout.ts
+++
@@ -1,6 +0,0 @@
-export const getLayoutFromSlug = (slug: string) => {
-  if (slug.includes("developers/docs")) return "docs"
-  if (slug.includes("developers/tutorials")) return "tutorial"
-  if (slug.includes("latest")) return "tutorial"
-  return "static"
-}
